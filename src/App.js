7e545130765d5f8cd46222ae31708840cbcdb248
Oscar Villagran <oscar.villagran@hotmail.com>
2023-05-22 17:46:33 -0500

    Add linters config

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <span>
           <span>Learn </span>
@@ -40,7 +38,8 @@ function App() {
           >
             Redux Toolkit
           </a>
-          ,<span> and </span>
+          ,
+          <span> and </span>
           <a
             className="App-link"
             href="https://react-redux.js.org/"
