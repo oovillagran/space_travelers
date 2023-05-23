a68d4d61c41fd87afa519079d73f5b795fc68e94
Osman Alhassan <oalhassan847@gmail.com>
2023-05-23 18:45:21 +0000

    Fetch Missions from endpoint and save to store

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import fetchMissions from '../../components/FetchMissions';
 
 const initialState = {
   missions: [],
@@ -8,6 +9,13 @@ export const missionsSlice = createSlice({
   name: 'missions',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMissions.fulfilled, (state, action) => ({
+        ...state,
+        missions: action.payload,
+      }));
+  },
 });
 
 export default missionsSlice.reducer;
