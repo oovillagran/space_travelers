4547aea6ebee48af4e9f2f62cddbd169fec4d2eb
Osman Alhassan <oalhassan847@gmail.com>
2023-05-23 16:32:26 +0000

    Create missionsSlice.js

--- src/components/Dragon.js
+++ src/components/Dragon.js
@@ -1,32 +1,29 @@
 import React from 'react';
 import '../assets/Dragon.css';
 
-const Dragon = () => {
-  console.log('here');
-  return (
-    <div className="dragon-container flex">
-      <img
-        className="img-dragon"
-        src="https://i.imgur.com/9fWdwNv.jpg"
-        alt="Dragon"
-      />
-      <div className="props-container flex">
-        <h2>Dragon 1</h2>
-        <div className="type-container flex">
-          <button type="button" className="reserved">
-            Reserved
-          </button>
-          <p className="txt-type">
-            <strong>Type: </strong>
-            capsule
-          </p>
-        </div>
-        <button type="button" className="Reserve">
-          Reserve Rocket
+const Dragon = () => (
+  <div className="dragon-container flex">
+    <img
+      className="img-dragon"
+      src="https://i.imgur.com/9fWdwNv.jpg"
+      alt="Dragon"
+    />
+    <div className="props-container flex">
+      <h2>Dragon 1</h2>
+      <div className="type-container flex">
+        <button type="button" className="reserved">
+          Reserved
         </button>
+        <p className="txt-type">
+          <strong>Type: </strong>
+          capsule
+        </p>
       </div>
+      <button type="button" className="Reserve">
+        Reserve Rocket
+      </button>
     </div>
-  );
-};
+  </div>
+);
 
 export default Dragon;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import dragonsReducer from './dragons/dragonsSlice';
+import missionsReducer from './missions/missionsSlice';
 
 const store = configureStore({
   reducer: {
     dragons: dragonsReducer,
+    missions: missionsReducer,
   },
 });
 
